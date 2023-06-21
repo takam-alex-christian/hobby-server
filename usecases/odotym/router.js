94ea1a2dc1a1acc1576148e954962220fc60bdaf
takam <takamalexchristian@gmail.com>
2023-06-21 20:23:29 +0800

    updates

--- usecases/odotym/router.js
+++ usecases/odotym/router.js
@@ -5,16 +5,29 @@ const express = require("express")
 
 const odotymRouter = express.Router();
 
-const {ToDoItemsModel} = require("./models")
+const { ToDoItemsModel } = require("./models")
 
-odotymRouter.post("/todo", (req, res)=>{
+odotymRouter.get("/todo", async (req, res)=>{
+    //n for the number limit
+    //s for the starting point
     
+    await ToDoItemsModel.find({}).then((docs)=>{
+        res.json(docs)
+    })
+
+    
+})
+
+odotymRouter.post("/todo", (req, res) => {
+
 
     ToDoItemsModel.create({
-        value: req.body.value? req.body.value: ""
-    }).then((doc)=>{
-        res.json({completed: true, doc: doc})
+        value: req.body.value ? req.body.value : ""
+    }).then((doc) => {
+        res.json({ completed: true, doc: doc })
     });
 })
+
+
 
 module.exports = odotymRouter;
